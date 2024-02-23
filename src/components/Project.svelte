daa422029093fbbe8c21548450a5eb640d307f5d
Martin M. <48408674+MartinTintin3@users.noreply.github.com>
2024-02-23 03:48:31 +0000

    Initial commit

---
+++ src/components/Project.svelte
@@ -0,0 +1,26 @@
+<script>
+    /** @type {string} */
+    export let background_url;
+    /** @type {string} */
+    export let title;
+    /** @type {string} */
+    export let description;
+    /** @type {string} */
+    export let link;
+</script>
+
+<div class="project">
+    <h2>{title}</h2>
+    <p>{description}</p>
+    <a href={link}>View project</a>
+</div>
+
+<style>
+    .project {
+        background: url({background_url});
+        background-size: cover;
+        padding: 20px;
+        border-radius: 5px;
+        border: 1px solid #000;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Project from '../components/Project.svelte';
+</script>
+
+<h1>Some of my stuff</h1>
+
+<Project
+	background_url="https://placekitten.com/200/300"
+	title="Kittens"
+	description="A collection of cute kittens"
+	link="/kittens" />
+
+<style>
+	* {
+		text-align: left;
+		margin: 0;
+	}
+</style>
