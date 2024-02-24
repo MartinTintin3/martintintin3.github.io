07a49d3b79ab0ed9ce2055614755cfcb1c24779e
MartinTintin3 <maroymart@gmail.com>
2024-02-23 22:08:23 -0500

    Added conway's game of life

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,6 +24,13 @@
 	title="Snake"
 	link={base_url + "/snake"}
 	description="Simple snake game (I was bored)" />
+
+	<Project
+	image_url={base_url + "/game-of-life/favicon.png"}
+	title="Conway's Game of Life"
+	link={base_url + "/game-of-life"}>
+	A cellular automation created by <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank">John Horton Conway</a>
+	</Project>
 </div>
 
 <style>
